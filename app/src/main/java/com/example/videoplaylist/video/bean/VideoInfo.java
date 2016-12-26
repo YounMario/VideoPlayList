073ec737cc9f0569919a6d4903acd0b7c2de2eff
yinlongquan <yinlongquan@conew.com>
2016-12-26 18:20:26 +0800

    彻底移除adapter 中控制ui 的逻辑

--- app/src/main/java/com/example/videoplaylist/video/bean/VideoInfo.java
+++ app/src/main/java/com/example/videoplaylist/video/bean/VideoInfo.java
@@ -2,54 +2,36 @@ package com.example.videoplaylist.video.bean;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.text.TextUtils;
-
-import java.io.File;
 
 public class VideoInfo implements Parcelable {
 
-    public static final int STATE_NOT_PLAY = 0;
-    public static final int STATE_PLAYED = 1;
-
 
     private long id;
-    private String videoId;
-    private String videoUrl;//视频的url
-    private String thumbnailUrl;//视频缩略图 url
-    private String md5;
+    private String videoUrl;
+    private String thumbnailUrl;
     private String title;
     private String desc;
-    private int type;//视频类型 ：0 普通 1 广告
-    private int categoryId;//视频分类 默认0
-    private String videoLocalPath;//【本地字段】视频缓存路径
+    private String videoLocalPath;
 
     public VideoInfo() {
     }
 
     private VideoInfo(Parcel in) {
         this.id = in.readLong();
-        this.videoId = in.readString();
         this.videoUrl = in.readString();
         this.thumbnailUrl = in.readString();
-        this.md5 = in.readString();
         this.title = in.readString();
         this.desc = in.readString();
-        this.type = in.readInt();
-        this.categoryId = in.readInt();
         this.videoLocalPath = in.readString();
     }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeLong(id);
-        dest.writeString(videoId);
         dest.writeString(videoUrl);
         dest.writeString(thumbnailUrl);
-        dest.writeString(md5);
         dest.writeString(title);
         dest.writeString(desc);
-        dest.writeInt(type);
-        dest.writeInt(categoryId);
         dest.writeString(videoLocalPath);
     }
 
@@ -59,14 +41,6 @@ public class VideoInfo implements Parcelable {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getVideoId() {
-        return videoId;
-    }
-
-    public void setVideoId(String videoId) {
-        this.videoId = videoId;
     }
 
     public String getVideoUrl() {
@@ -85,14 +59,6 @@ public class VideoInfo implements Parcelable {
         this.thumbnailUrl = thumbnailUrl;
     }
 
-    public String getMd5() {
-        return md5;
-    }
-
-    public void setMd5(String md5) {
-        this.md5 = md5;
-    }
-
     public String getTitle() {
         return title;
     }
@@ -109,42 +75,18 @@ public class VideoInfo implements Parcelable {
         this.desc = desc;
     }
 
-    public int getType() {
-        return type;
-    }
-
-    public void setType(int type) {
-        this.type = type;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
     public String getVideoLocalPath() {
         return videoLocalPath;
-    }
-
-    public void setVideoLocalPath(String videoLocalPath) {
-        this.videoLocalPath = videoLocalPath;
     }
 
     @Override
     public String toString() {
         return "VideoInfo{" +
                 "id=" + id +
-                ", videoId =" + videoId +
                 ", videoDownloadUrl = " + videoUrl +
                 ", thumbnailUrl =" + thumbnailUrl +
-                ", md5='" + md5 +
                 ", title = " + title +
                 ", desc = " + desc +
-                ", type = " + type +
-                ", categoryId =" + categoryId +
                 ", videoLocalPath='" + videoLocalPath +
                 '}';
     }
@@ -164,18 +106,6 @@ public class VideoInfo implements Parcelable {
     @Override
     public int describeContents() {
         return 0;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) return false;
-        VideoInfo temp = (VideoInfo) obj;
-        return videoId.equalsIgnoreCase(temp.getVideoId());
-    }
-
-    @Override
-    public int hashCode() {
-        return videoId.hashCode();
     }
 
 }
